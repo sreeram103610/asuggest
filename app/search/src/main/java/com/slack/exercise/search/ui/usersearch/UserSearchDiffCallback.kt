8e1d559034c1ab6019f1e3a77e9c953b165c1214
sree <sreeram103610@gmail.com>
2023-10-05 17:36:28 -0500

    Modify diff logic to use ids instead of usernames

--- app/search/src/main/java/com/slack/exercise/search/ui/usersearch/UserSearchDiffCallback.kt
+++ app/search/src/main/java/com/slack/exercise/search/ui/usersearch/UserSearchDiffCallback.kt
@@ -13,10 +13,10 @@ class UserSearchDiffCallback(
     override fun getNewListSize(): Int = newList.size
 
     override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-        return oldList[oldItemPosition].username == newList[newItemPosition].username
+        return oldList[oldItemPosition] == newList[newItemPosition]
     }
 
     override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-        return oldList[oldItemPosition] == newList[newItemPosition]
+        return oldList[oldItemPosition].id == newList[newItemPosition].id
     }
 }
