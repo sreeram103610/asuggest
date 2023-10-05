3a23004cb359c6d83b5bb57b4af472e4423e7430
sree <sreeram103610@gmail.com>
2023-10-05 16:51:58 -0500

    extract DomainResult to separate file

---
+++ app/search/src/main/java/com/slack/exercise/search/domain/model/DomainResult.kt
@@ -0,0 +1,11 @@
+package com.slack.exercise.search.domain.model
+
+sealed class DomainResult<out T> {
+    object Loading : DomainResult<Nothing>()
+    data class Loaded<out T>(val data: T): DomainResult<T>()
+    data class Error(val type: DomainErrorType) : DomainResult<Nothing>()
+}
+
+interface DomainErrorType
+object InternetError: DomainErrorType
+object NoUsersFound: DomainErrorType
--- app/search/src/main/java/com/slack/exercise/search/domain/usecase/GetUsersUsecase.kt
+++ app/search/src/main/java/com/slack/exercise/search/domain/usecase/GetUsersUsecase.kt
@@ -4,6 +4,9 @@ import com.slack.exercise.search.data.repo.BlockedPhrasesRepo
 import com.slack.exercise.search.data.repo.SearchRepo
 import com.slack.exercise.search.data.repo.model.RepoErrorType
 import com.slack.exercise.search.data.repo.model.RepoResult
+import com.slack.exercise.search.domain.model.DomainResult
+import com.slack.exercise.search.domain.model.InternetError
+import com.slack.exercise.search.domain.model.NoUsersFound
 import com.slack.exercise.search.domain.model.UserSearchResult
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.emitAll
@@ -17,18 +20,19 @@ interface GetUsersUsecase {
 
 internal class DefaultGetUsersUsecase @Inject constructor(private val searchRepo: SearchRepo, private val blockedPhrasesRepo: BlockedPhrasesRepo) : GetUsersUsecase {
     override fun invoke(searchItem: String): Flow<DomainResult<List<UserSearchResult>>> {
+        val validatedSearchTerm = searchItem.trim().lowercase()
         return flow {
             emit(DomainResult.Loading)
-            if (blockedPhrasesRepo.phraseExists(searchItem)) {
+            if (blockedPhrasesRepo.phraseExists(validatedSearchTerm)) {
                 emit(DomainResult.Error(NoUsersFound))
             }
             else {
-                emitAll(searchRepo.searchUsers(searchItem)
+                emitAll(searchRepo.searchUsers(validatedSearchTerm)
                     .map {
                         when (it) {
                             is RepoResult.Available -> {
                                 if (it.repoData.isEmpty()) {
-                                    blockedPhrasesRepo.addPhrase(searchItem)
+                                    blockedPhrasesRepo.addPhrase(validatedSearchTerm)
                                     DomainResult.Error(NoUsersFound)
                                 }
                                 else {
@@ -48,13 +52,3 @@ internal class DefaultGetUsersUsecase @Inject constructor(private val searchRepo
     }
 }
 
-sealed class DomainResult<out T> {
-    object Loading : DomainResult<Nothing>()
-    data class Loaded<out T>(val data: T): DomainResult<T>()
-    data class Error(val type: DomainErrorType) : DomainResult<Nothing>()
-}
-
-interface DomainErrorType
-
-object InternetError: DomainErrorType
-object NoUsersFound: DomainErrorType
