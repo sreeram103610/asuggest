950fcd3824df84e60bb7d1e1409f3ad3a5eac9c2
sree <sreeram103610@gmail.com>
2023-10-05 16:49:14 -0500

    Add image loader as singleton to SearchModule

--- app/search/src/main/java/com/slack/exercise/search/di/SearchModule.kt
+++ app/search/src/main/java/com/slack/exercise/search/di/SearchModule.kt
@@ -1,29 +1,30 @@
 package com.slack.exercise.search.di
 
-import com.slack.exercise.search.data.api.DefaultBlockedPhrasesFileApi
-import com.slack.exercise.search.data.api.SlackSearchClient
-import com.slack.exercise.search.data.api.DefaultSlackSearchClient
+import android.content.Context
+import coil.ImageLoader
+import coil.disk.DiskCache
+import coil.memory.MemoryCache
+import com.slack.exercise.search.BuildConfig.IMAGE_CACHE_MAX_MEMORY_PERCENT
+import com.slack.exercise.search.BuildConfig.IMAGE_CACHE_NAME
 import com.slack.exercise.search.data.api.BlockedPhrasesFileApi
+import com.slack.exercise.search.data.api.DefaultBlockedPhrasesFileApi
+import com.slack.exercise.search.data.api.DefaultSlackSearchClient
 import com.slack.exercise.search.data.api.SlackSearchApi
-import com.slack.exercise.search.data.model.UserDto
+import com.slack.exercise.search.data.api.SlackSearchClient
 import com.slack.exercise.search.data.repo.BlockedPhrasesRepo
 import com.slack.exercise.search.data.repo.DefaultBlockedPhrasesRepo
 import com.slack.exercise.search.data.repo.DefaultSearchRepo
 import com.slack.exercise.search.data.repo.SearchRepo
 import com.slack.exercise.search.data.util.DefaultSearchCache
 import com.slack.exercise.search.data.util.SearchCache
-import com.slack.exercise.search.domain.dataprovider.UserSearchResultDataProvider
-import com.slack.exercise.search.domain.dataprovider.UserSearchResultDataProviderImpl
 import com.slack.exercise.search.ui.usersearch.UserSearchFragment
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.android.ContributesAndroidInjector
-import io.github.reactivecircus.cache4k.Cache
 import retrofit2.Retrofit
 import javax.inject.Scope
 import javax.inject.Singleton
-import kotlin.time.Duration.Companion.minutes
 
 @Module(includes = [DomainModule::class, SearchModule.Provider::class])
 abstract class SearchModule {
@@ -42,6 +43,23 @@ abstract class SearchModule {
         @Singleton
         @Provides
         fun provideOfflineCache() : SearchCache = DefaultSearchCache
+
+        @Singleton
+        @Provides
+        fun provideImageLoader(context: Context) : ImageLoader {
+            return ImageLoader.Builder(context)
+                .memoryCache {
+                    MemoryCache.Builder(context)
+                        .maxSizePercent(IMAGE_CACHE_MAX_MEMORY_PERCENT)
+                        .build()
+                }
+                .diskCache {
+                    DiskCache.Builder()
+                        .directory(context.cacheDir.resolve(IMAGE_CACHE_NAME))
+                        .build()
+                }
+                .build()
+        }
     }
 
     @Singleton
